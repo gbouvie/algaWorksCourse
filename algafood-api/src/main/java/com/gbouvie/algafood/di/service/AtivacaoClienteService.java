d8501f008225017896717b8469a1ba81a5986ed2
Giovani Bouvie <gbouvie@gmail.com>
2023-03-16 14:22:28 -0300

    Especialista Spring REST - Aula 2.14

--- algafood-api/src/main/java/com/gbouvie/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/gbouvie/algafood/di/service/AtivacaoClienteService.java
@@ -1,24 +1,36 @@
 package com.gbouvie.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.gbouvie.algafood.di.modelo.Cliente;
 import com.gbouvie.algafood.di.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 
+	@Autowired
 	private Notificador notificador;
 
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService: " + notificador);
-	}
+//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+	
+//	public AtivacaoClienteService(String qualquer) {
+//		
+//	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
 		this.notificador.notificar(cliente, "Seu cadastro no sistema foi ativado!");
 	}
+
+//	@Autowired
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
 	
 }
+ 
