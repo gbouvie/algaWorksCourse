a5ac46ca1aefa4ed8e7705b0bdc8af953c57c6eb
Giovani Bouvie <gbouvie@gmail.com>
2023-03-17 11:52:53 -0300

    Especialista Sprint REST - Aula 2.25

--- algafood-api/src/main/java/com/gbouvie/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/gbouvie/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.gbouvie.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +11,21 @@ import com.gbouvie.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 
+	@Value("${notificador.email.host-smtp}")
+	private String host;
+	
+	@Value("${notificador.email.porta-smtp}")
+	private Integer porta;
+	
 	public NotificadorEmail() {
 		System.out.println("Notificador email REAL");
 	}
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host: " + host);
+		System.out.println("Porta: " + porta);
+		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 	
