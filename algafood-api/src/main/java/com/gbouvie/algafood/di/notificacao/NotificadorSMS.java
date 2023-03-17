56c50da9e280dda63ed7ea79238aece982cfb69d
Giovani Bouvie <gbouvie@gmail.com>
2023-03-17 10:03:35 -0300

    Especialista Spring REST - Aula 2.18

--- algafood-api/src/main/java/com/gbouvie/algafood/di/notificacao/NotificadorSMS.java
+++ algafood-api/src/main/java/com/gbouvie/algafood/di/notificacao/NotificadorSMS.java
@@ -1,11 +1,11 @@
 package com.gbouvie.algafood.di.notificacao;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.gbouvie.algafood.di.modelo.Cliente;
 
-@Primary
+@Qualifier("urgente")
 @Component
 public class NotificadorSMS implements Notificador {
 
