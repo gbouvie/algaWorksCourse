d8499493ec65c23c9a7f96b93b42bdb1eea59a2f
Giovani Bouvie <gbouvie@gmail.com>
2023-03-21 17:03:54 -0300

    Especialista Spring REST - Aula 3.8

---
+++ algafood-api/src/main/java/com/gbouvie/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,24 @@
+package com.gbouvie.algafood.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Component;
+
+import com.gbouvie.algafood.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public List<Cozinha> listar() {
+		return manager.createQuery("from Cozinha", Cozinha.class)
+				.getResultList();
+	}
+	
+}
---
+++ algafood-api/src/main/java/com/gbouvie/algafood/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,29 @@
+package com.gbouvie.algafood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.gbouvie.algafood.AlgafoodApiApplication;
+import com.gbouvie.algafood.domain.model.Cozinha;
+
+public class ConsultaCozinhaMain {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
+		
+		for (Cozinha cozinha : cozinhas) {
+			System.out.println(cozinha.getNome());
+		}
+	}
+	
+}
