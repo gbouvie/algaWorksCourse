812dc329edd510917b15b3db3f9821c95317d7df
Giovani Bouvie <gbouvie@gmail.com>
2023-04-12 22:59:02 -0300

    Especialista Spring REST - Aula 3.9

--- algafood-api/src/main/java/com/gbouvie/algafood/jpa/CadastroCozinha.java
+++ algafood-api/src/main/java/com/gbouvie/algafood/jpa/CadastroCozinha.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Component;
 
@@ -21,4 +21,8 @@ public class CadastroCozinha {
 				.getResultList();
 	}
 	
+	@Transactional
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);
+	}
 }
