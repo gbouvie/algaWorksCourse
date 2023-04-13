963372ded988c66c64c7495f6ab998a91e209c7a
Giovani Bouvie <gbouvie@gmail.com>
2023-04-12 23:03:46 -0300

    Especialista Spring REST - Aula 3.10

---
+++ algafood-api/src/main/java/com/gbouvie/algafood/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,25 @@
+package com.gbouvie.algafood.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.gbouvie.algafood.AlgafoodApiApplication;
+import com.gbouvie.algafood.domain.model.Cozinha;
+
+public class BuscaCozinhaMain {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		Cozinha cozinha = cadastroCozinha.buscar(1L);
+		
+		System.out.println(cozinha.getNome());
+	}
+	
+}
