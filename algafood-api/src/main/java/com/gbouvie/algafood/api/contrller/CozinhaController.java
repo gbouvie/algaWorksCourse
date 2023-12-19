f35f3c1433b959629254b2e679b2fbb649cebdd2
Giovani Bouvie <gbouvie@gmail.com>
2023-12-18 22:34:35 -0300

    Especialista Spring REST - Aula 4.10

---
+++ algafood-api/src/main/java/com/gbouvie/algafood/api/contrller/CozinhaController.java
@@ -0,0 +1,25 @@
+package com.gbouvie.algafood.api.contrller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gbouvie.algafood.domain.model.Cozinha;
+import com.gbouvie.algafood.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping
+	public List<Cozinha> listar() {
+		return cozinhaRepository.listar();
+	}
+	
+}
