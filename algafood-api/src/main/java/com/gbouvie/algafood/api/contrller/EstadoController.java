68624626b73f9020f911a308288b7945271d0d47
Giovani Bouvie <gbouvie@gmail.com>
2024-01-17 22:13:17 -0300

    Especialista Sprint REST - Aula 4.11

---
+++ algafood-api/src/main/java/com/gbouvie/algafood/api/contrller/EstadoController.java
@@ -0,0 +1,24 @@
+package com.gbouvie.algafood.api.contrller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gbouvie.algafood.domain.model.Estado;
+import com.gbouvie.algafood.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@GetMapping
+	public List<Estado> listar() {
+		return estadoRepository.listar();
+	}
+}
