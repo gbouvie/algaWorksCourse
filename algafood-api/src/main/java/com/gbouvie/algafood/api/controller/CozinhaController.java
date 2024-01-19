75cc38007b3fb1e3eba820f79caeb3f71454bcdd
Giovani Bouvie <gbouvie@gmail.com>
2024-01-18 22:02:19 -0300

    Especialista Spring REST - Aula 4.16

--- algafood-api/src/main/java/com/gbouvie/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/gbouvie/algafood/api/controller/CozinhaController.java
@@ -1,13 +1,15 @@
-package com.gbouvie.algafood.api.contrller;
+package com.gbouvie.algafood.api.controller;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gbouvie.algafood.api.model.CozinhasXmlWrapper;
 import com.gbouvie.algafood.domain.model.Cozinha;
 import com.gbouvie.algafood.domain.repository.CozinhaRepository;
 
@@ -19,11 +21,16 @@ public class CozinhaController {
 	private CozinhaRepository cozinhaRepository;
 	
 	@GetMapping
-	public List<Cozinha> listar1() {
+	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
 	
-	@GetMapping(value = "/{cozinhaId}")
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}
+	
+	@GetMapping("/{cozinhaId}")
 	public Cozinha buscar(@PathVariable Long cozinhaId) {
 		return cozinhaRepository.buscar(cozinhaId);
 	}
--- algafood-api/src/main/java/com/gbouvie/algafood/api/controller/EstadoController.java
+++ algafood-api/src/main/java/com/gbouvie/algafood/api/controller/EstadoController.java
@@ -1,4 +1,4 @@
-package com.gbouvie.algafood.api.contrller;
+package com.gbouvie.algafood.api.controller;
 
 import java.util.List;
 
