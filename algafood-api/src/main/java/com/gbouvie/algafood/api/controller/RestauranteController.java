98d51e706d1a3932a4cc5d899650f29dcf7b8045
Giovani Bouvie <gbouvie@gmail.com>
2024-03-24 16:34:38 -0300

    Especialista Spring REST - Aula 4.34

--- algafood-api/src/main/java/com/gbouvie/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/gbouvie/algafood/api/controller/RestauranteController.java
@@ -1,5 +1,6 @@
 package com.gbouvie.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gbouvie.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.gbouvie.algafood.domain.model.Restaurante;
 import com.gbouvie.algafood.domain.repository.RestauranteRepository;
@@ -95,9 +98,21 @@ public class RestauranteController {
 		return atualizar(restauranteId, restauranteAtual);
 	}
 
-	private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
-		camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			System.out.println(nomePropriedade + " - " + valorPropriedade);
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+		
+		System.out.println(restauranteOrigem);
+		
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+			field.setAccessible(true);
+			
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			
+//			System.out.println(nomePropriedade + " - " + valorPropriedade + " = " + novoValor);
+			
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
 		});
 	}
 }
